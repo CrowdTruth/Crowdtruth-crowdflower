557d35b17daaef3f18adfff59cc1fff267448d2b
Carlos Martinez <c.martinez@esciencecenter.nl>
2014-09-16 09:56:55 +0200

    Update API key

--- src/config/template.php
+++ src/config/template.php
@@ -2,7 +2,7 @@
 
 return array(
  
-	'apikey' => '',
+	'apikey' => 'H_MX6QhTNBmSE-zdsqd1',
 	'webhookuri' => Request::root() . '/cfwebhook.php',
 	'englishcountries' => '["GB","US", "CA", "AU"]',
 	'customcountries' => array('language' => 'Dutch', 'countries' => '["BE", "AN", "NL", "SR"]'),
